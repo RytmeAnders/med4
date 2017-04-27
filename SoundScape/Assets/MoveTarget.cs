6a16ad97ced9052e82c51975e4d5cd19e12a0c8c
Senpai <lohsea@hotmail.com>
2017-04-27 14:09:12 +0200

    Unity Project Imbound! <3

---
+++ SoundScape/Assets/MoveTarget.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTarget : MonoBehaviour {
+
+
+    private int posCounter;
+
+    private GameObject target;
+    private Vector3[] targetPos = new Vector3[6];
+
+	// Use this for initialization
+	void Start () {
+        posCounter = -1;
+        target = GameObject.Find("Target");
+        target.transform.position = new Vector3(0f, 0f, 0f);
+        targetPos[0] = new Vector3(10f, 10f, 10f);
+        targetPos[1] = new Vector3(20f, 20f, 20f);
+        targetPos[2] = new Vector3(30f, 30f, 30f);
+        targetPos[3] = new Vector3(40f, 40f, 40f);
+        targetPos[4] = new Vector3(50f, 50f, 50f);
+        targetPos[5] = new Vector3(60f, 60f, 60f);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        posCounter++;
+        if (posCounter >= 6)
+        {
+            posCounter = 0;
+        }
+        target.transform.position = targetPos[posCounter];
+        print(posCounter);
+    }
+}
---
+++ SoundScape/Assets/ReadingArduino.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.IO.Ports;
+
+public class ReadingArduino : MonoBehaviour {
+
+    SerialPort stream;
+
+	// Use this for initialization
+	void Start () {
+        stream = new SerialPort("COM8", 9600);
+        stream.ReadTimeout = 50;
+        stream.Open();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(stream.IsOpen)
+        {
+            try
+            {
+                stream.ReadLine();
+            }
+            catch
+            {
+
+            }
+        }
+	}
+
+    public void WriteToArduino(string message)
+    {
+        stream.WriteLine(message);
+        stream.BaseStream.Flush();
+    }
+}
