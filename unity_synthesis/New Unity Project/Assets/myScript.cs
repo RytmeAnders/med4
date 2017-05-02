08e700462ece7896a6a0c78c00664780cb6aca22
RytmeAnders <aschristiansen@gmail.com>
2017-05-02 16:12:25 +0200

    LPF is now bound to acceleration also

--- unity_synthesis/New Unity Project/Assets/myScript.cs
+++ unity_synthesis/New Unity Project/Assets/myScript.cs
@@ -9,25 +9,34 @@ public class myScript : MonoBehaviour {
 
     public float cutoffOn = 800;
     public float cutoffOff = 100;
-    public int lowCut;
-    public int frequencyScalar;
+    public int lowCut;              //Minimum frequency for the LPF
+    public int frequencyScalar;     //A scalar for the changes, because they are too small for the LPF
 
     bool engineOn;
 
-    int speed;                      //for rotating the cube at different speeds
+    float speedRot;                 //for rotating the cube at different speeds
+    float speedAcc;                 //for acceleration of the cube
 
     float rotOld;                   //old rotation value
     float rotNew;                   //new rotation value
     float rotDiff;                  //old-new rotation
 
+    float avgDiff;                  //average between angle and acceleration
+
+    float accOld;                   //old accelerometer value
+    float accNew;                   //new accelerometer value
+    float accDiff;                  //new-old accelerometer value
+
     Rigidbody rb;
     Vector3 v3Velocity;
 
-    System.Random rand = new System.Random();
+    System.Random rand = new System.Random();               //A class with a method for generating random values
     AudioLowPassFilter lowPassFilter;
 
     void Awake() {                                          //INITIALIZE VARIABLES WHEN WAKING
         rotOld = transform.localEulerAngles.x;              //set rotOld to the x rotation on startup (0)
+        accOld = transform.localPosition.x;                 //set accOld to the local x position on startup
+
         rb = GetComponent<Rigidbody>();                     //Rigidbody object
         v3Velocity = rb.velocity;                           //Velocity of the rigidbody object
         lowPassFilter = GetComponent<AudioLowPassFilter>(); //Object of the LowPassFilter component
@@ -41,24 +50,45 @@ public class myScript : MonoBehaviour {
     }
 
     void Update() {
+        // ROTATIONAL DIFFERENCE --------------------
         rotNew = transform.localEulerAngles.x;              //New angle
         rotDiff = Mathf.Abs(rotOld - rotNew);               //Difference between old and new angle
-        if (rotDiff > speed) {                              //Using speed as threshold to avoid too high values
-            rotDiff = speed;
+        if (rotDiff > speedRot) {                           //Using speed as threshold to avoid too high values
+            rotDiff = speedRot;
         }
         rotOld = rotNew;                                    //Setting new rotation as the old one before reiterating
 
-        print(rotDiff);
-        lowPassFilter.cutoffFrequency = lowCut+rotDiff*frequencyScalar;     //cutoffFrequency changes based on the angular change
-
         if (Input.GetKey(KeyCode.W)) {                      //Rotate cube by speed = 10
-            speed = 10;
-            transform.Rotate(speed, 0, 0);
+            speedRot = 10;
+            transform.Rotate(speedRot, 0, 0);
         }
         if (Input.GetKey(KeyCode.E)) {                      //Rotate cube by speed = 5
-            speed = 5;
-            transform.Rotate(speed, 0, 0);
+            speedRot = 5;
+            transform.Rotate(speedRot, 0, 0);
         }
+        // END ROTATIONAL DIFFERENCE -----------------
+
+        // ACCELEROMETER DIFFERENCE ------------------
+        accNew = transform.localPosition.x;                 //(See rot example)
+        accDiff = Mathf.Abs(accNew - accOld);
+        if (accDiff > speedAcc) {
+            accDiff = speedAcc;
+        }
+        accOld = accNew;
+
+        if (Input.GetKey(KeyCode.S)) {                      //Move cube by speed = 2
+            speedAcc = 2;
+            transform.localPosition += new Vector3(speedAcc, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D)) {                      //Move cube by speed = 1
+            speedAcc = 1;
+            transform.localPosition -= new Vector3(speedAcc, 0, 0);
+        }
+        // END ACCELEROMTER DIFFERENCE ---------------
+
+        avgDiff = Mathf.Max(rotDiff, accDiff);              //LPF takes the highest value between angle and acceleration
+        lowPassFilter.cutoffFrequency = lowCut+avgDiff*frequencyScalar;     //cutoffFrequency changes based on movement
+        print(avgDiff); 
     }
 
     //lowPassFilter.cutoffFrequency = engineOn ? cutoffOn : cutoffOff;
