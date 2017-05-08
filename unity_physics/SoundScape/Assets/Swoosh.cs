c30c43b6452b3e50ee5ccf0ee6ab53f2ae57608f
RytmeAnders <aschristiansen@gmail.com>
2017-05-08 14:55:02 +0200

    Static Hand sound implemented (LPF=900)

--- unity_physics/SoundScape/Assets/Swoosh.cs
+++ unity_physics/SoundScape/Assets/Swoosh.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Swoosh : MonoBehaviour {
     //TODO Removed orientationNew, using just ard.orientation, should work
 
+    public bool dynamicSound;
+    int speedRot, speedAcc;
+
     [Range(-1f, 1f)]
     public float offset;                                    //offset slider with range -1:1 (range defined above)
 
@@ -14,7 +17,6 @@ public class Swoosh : MonoBehaviour {
     System.Random rand = new System.Random();               //A class with a method for generating random values
     AudioLowPassFilter lowPassFilter;
     ReadingArduino ard;
-
 
     void OnAudioFilterRead(float[] data, int channels) {                //White noise generation
         for (int i = 0; i < data.Length; i++) {
@@ -34,12 +36,22 @@ public class Swoosh : MonoBehaviour {
         orientationDiff = Mathf.Abs(ard.orientation - orientationOld);
         orientationOld = ard.orientation;
 
-        /*TODO Maybe this prevents clipping?
-        if(orientationDiff > ard.orientation) {
-            orientationDiff = Mathf.Abs(ard.orientation);
-        }*/
+        if (dynamicSound) {
+            /*TODO Maybe this prevents clipping?
+            if(orientationDiff > ard.orientation) {
+                orientationDiff = Mathf.Abs(ard.orientation);
+            }*/
 
-        print("Difference: " + orientationDiff + " Raw Value: " + ard.orientation);
-        lowPassFilter.cutoffFrequency = lowCut + orientationDiff * scalar;          //LPF freq changing based on change in orientation
+            print("Difference: " + orientationDiff + " Raw Value: " + ard.orientation);
+            lowPassFilter.cutoffFrequency = lowCut + orientationDiff * scalar;          //LPF freq changing based on change in orientation
+        }
+        else {
+            if (orientationDiff > 2f) {                 //If difference is > 2, play a static sound sample
+                print("Sound Activated!");
+                lowPassFilter.cutoffFrequency = 900;    //Static because cutOff is always 900
+            }
+            print("Sound Inactive!");
+            lowPassFilter.cutoffFrequency = 0;          //If no difference over time, filter out all freqs (play no sound)
+        }
     }
 }
